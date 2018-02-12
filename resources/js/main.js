a9660f1ebc8045f6babe657a622be5f7c518b09f
Paddlefish <claireelisemason@gmail.com>
2018-02-12 19:44:27 +1100

    Partially complete

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+});
+
+var scrolldistance = 0
